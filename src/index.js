8720af64484b8d9b5f3f3103afa844f19c1f862a
DmitriLyalin <donlyalin@gmail.com>
2024-08-18 20:15:36 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const stringNumber = Math.abs(n).toString();
+    let reverseString = "";
+    for (let i = stringNumber.length-1; i >= 0; i--) {
+      reverseString = `${reverseString}${stringNumber[i]}`;
+    }
+    return Number(reverseString);
 }
